ff630aab5ce8bfa88fa79f858abe87787817b706
Dany-CMB1 <daniel.bannister@proton.me>
2023-05-31 13:19:04 +0200

    Fichiers annexes

--- src/main.cpp
+++ src/main.cpp
@@ -5,136 +5,128 @@
 //Déclaration pins module BT
 #define statePin 2
 #define rxPin 3 //Pin Rx du HC05 --> Pin 3 de l'Arduino
-#define txPin 4//Pin Tx du HC05 --> Pin 4 de l'Arduino
-#define keyPin 5
+#define txPin 7//Pin Tx du HC05 --> Pin 4 de l'Arduino
+#define keyPin 8
 
-//Définition des macros: on remplace les msgs texte par des entiers dans les protocoles de communication
 #define CALIBRAGE_TEMPS 5
 #define CALIBRAGE_POT 6
 #define TOP_DEPART 7
-
-//Déclaration des pins sur lesquels les capteurs sont branchés
-int GLED = 4;
-int Laser = 2;
-int LightSensor= 0;
-int Pot = 5;
+#define FIN_COURSE 8
+#define hc05BR 38400
+#define nanoBR 9600
 
 int directionPin = 12;
 int pwmPin = 3;
 int brakePin = 9;
-
-//uncomment if using channel B, and remove above definitions
-//int directionPin = 13;
-//int pwmPin = 11;
-//int brakePin = 8;
-
-//boolean to switch direction
-bool directionState;
-
+//Déclaration des pins sur lesquels les capteurs sont branchés
+int GLED = 4;
+int Laser = 6;
+int LightSensor = 2;
+int Pot = 5;
+LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
 //Initialisation des variables
 bool currStatus;
 bool prevStatus;
+int val_brute; 
 bool chrono = 0;
-int nb_fd, nb_fm;
-int seuil = 0, pot = 0;
+int nb_fd=0, nb_fm=0;
+int seuil = 600, pot = 0;
 double tps, start;
-unsigned long time_offset; //Décalage temporel (millis()) de la Uno par rapport à la nano
 
+unsigned long millis_nano;
 SoftwareSerial hc05(txPin, rxPin); //Tx | Rx pdv HC05
-LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
+bool directionState;
 
 void setup()
-{
+{ digitalWrite(pwmPin, LOW);
+  digitalWrite(brakePin, HIGH);
+  //Initialisation de l'écran LCD 
+  lcd.init(); // initialize the lcd
+  lcd.backlight();
+  lcd.clear();                 // clear display
+  lcd.setCursor(0, 0);         // move cursor to   (0, 0)
+  lcd.print("Depart course");
   // Init Serial at 115200 Baud Rate 
-  Serial.begin(115200); 
-
-  pinMode(directionPin, OUTPUT); digitalWrite(directionPin,HIGH);
-  pinMode(pwmPin, OUTPUT);
-  pinMode(brakePin, OUTPUT); digitalWrite(brakePin, LOW);
-
+  Serial.begin(9600); 
+  hc05.begin(hc05BR);
   //Déclaration de l'état des capteurs digitaux (entrée ou sortie)
-  pinMode(Laser, OUTPUT); digitalWrite(Laser, HIGH); 
+  pinMode(Laser, OUTPUT); 
+  digitalWrite(Laser, HIGH); 
   
-  //Initialisation LED de fonctionnement
   pinMode(GLED, OUTPUT); 
   digitalWrite(GLED, LOW); 
+  //calibratePot();
+  lcd.clear();                 // clear display
+  lcd.setCursor(0, 0);         // move cursor to   (0, 0)
+  lcd.print("Seuil :");
+  lcd.setCursor(0, 8);
+  lcd.print(seuil);
 
-  //Initialisation de l'écran LCD 
-  lcd.init(); 
-  lcd.backlight();
-  lcd.clear();   
-
-  //Calibrage temps et potard
-  calibratePot();
-  calibrateTime();
-
-  digitalWrite(pwmPin, 100);    
-
+  pinMode(directionPin, OUTPUT);
+  pinMode(pwmPin, OUTPUT);
+  pinMode(brakePin, OUTPUT);
+  delay(2000);
+  digitalWrite(pwmPin, HIGH);
+  digitalWrite(brakePin, LOW);
 }
 
 
 void loop()
 {
+  //Serial.println(seuil);
   //Acquisition des états logiques des BO (1: passant, 0: bloqué)
   GetSensorStatus();
 
-  //Màj du nb de fm et fd
   //Détection des fd
   if (!currStatus && prevStatus) {nb_fd++;}
 
   //Détection des fm
-  if (currStatus  && !prevStatus ) {nb_fm ++;}
+  else if (currStatus && !prevStatus) {nb_fm++;}
 
-  //Etat de base: attente du départ
-  if (!chrono) Serial.println("Attente départ");
+  if(hc05.available() && hc05.read()==TOP_DEPART){
+    chrono=1;
+    digitalWrite(GLED, HIGH);
+    start=(millis());
+    nb_fd=0;
+    }
 
-  //Mesure de l'écoulement du temps:
-  if (chrono) {tps=(time_offset + millis()-start)/(double)1000; Serial.println(tps); }
-
-  //Détection du 1er front montant de la BO de départ; démarrage du chrono
-  else if (hc05.available() && hc05.read()==2 && !chrono) {chrono = 1; start = time_offset + millis(); digitalWrite(GLED, HIGH);}
-
-  //Détection du 1er front descendant de la BO d'arrivée; arrêt du chrono, affichage continu du temps final
-  if (nb_fd==1 && chrono) {chrono = 0; digitalWrite(GLED, LOW); digitalWrite(pwmPin,0); LCDprintTime();}
-
+  if (chrono) {
+    //Serial.println(tps);
+    tps=(millis()-start)/(double)1000; 
+    lcd.clear();                 // clear display
+    lcd.setCursor(0, 0);         // move cursor to   (0, 0)
+    lcd.print(tps); }
+    
+  if (nb_fd>=1 && chrono) {
+    digitalWrite(pwmPin, LOW);
+    digitalWrite(brakePin, HIGH);
+    chrono = 0; 
+    digitalWrite(GLED, LOW);
+    lcd.clear();                 // clear display
+    lcd.setCursor(0, 0);         // move cursor to   (0, 0)
+    lcd.print(tps);
+    nb_fd=0;
+    nb_fm=0;
+    hc05.write(FIN_COURSE);}
+  //Serial.println(nb_fd);
 }
 
-void LCDprintTime()
-{
-  lcd.setCursor(0,0);
-  lcd.print(tps);
-}
+
 
 void GetSensorStatus()
 {
   prevStatus = currStatus;
   //Lecture des valeurs analogiques des photodiodes
   //Conversion analogique --> logique
-  if (analogRead(LightSensor)>seuil) currStatus =1; 
-  else currStatus =0; 
+  if (analogRead(LightSensor)>seuil) currStatus=1; 
+  else currStatus=0; 
+  Serial.println(analogRead(LightSensor));
 }
 
-
-void calibrateTime()
-{
-  //Attente synchronisation demande calibrage temps
-  while(!(hc05.available() && hc05.read()==CALIBRAGE_TEMPS)){hc05.write(CALIBRAGE_TEMPS);}
-  
-  //Attente réponse 
-  while(!hc05.available()){}
-
-  //Lecture de la valeur millis() de la nano
-  for (int i=0;i<sizeof(int); i++) {if (hc05.available()) time_offset += hc05.read()<<(sizeof(int)*i);}
-  time_offset -= millis();
- }
-
- void calibratePot()
+void calibratePot()
  {
-  //Attente synchronisation demande régalge potentiomètre
-  while(!(hc05.available() && hc05.read()==CALIBRAGE_POT)){hc05.print(CALIBRAGE_POT);}
-
-  //Réglage du seuil en fonction du potentiomètre
-  pot = 1023 - analogRead(Pot); 
-  seuil = map(pot, 0, 1023, 0, 1000); 
-  hc05.write(seuil);
+  pot = 1023 - analogRead(Pot);
+  seuil = map(pot, 0, 1023, 0, 1000);
+  //String seuilStr = String(seuil);
+  //hc05.println(seuilStr);
  }
